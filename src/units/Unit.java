60b08f17029700a27358e16831206dadc9301b75
Rakhyvel <josephmshimel@gmail.com>
2021-04-20 19:12:59 -0700

    Add UnitType

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -1,5 +1,53 @@
+
 package units;
 
-public class Unit {
+public abstract class Unit {
+	private String nation;
+	private int posX, posY;
+	private final UnitType type;
+
+	public Unit(String nation, int posX, int posY, UnitType type) {
+		this.nation = nation;
+		this.posX = posX;
+		this.posY = posY;
+		this.type = type;
+	}
+
+	// Called when unit needs to move to a new tile
+	public abstract void move(int x, int y);
+
+	// Called when the unit will attack another tile
+	public abstract void attack(int x, int y);
+
+	// Called when unit defends
+	public abstract void defend();
+
+	public String getNation() {
+		return nation;
+	}
+
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public UnitType getType() {
+		return type;
+	}
 
 }
