5a09be9ed6761f31d91be3a24a6324be434d6c2c
Adi-UA <abanerjee@email.arizona.edu>
2021-04-29 23:53:49 -0700

    The model now updates the view and tells it how to render the map Two buttons added that lets the user select what they are trying to do this turn The controller move method now returns true or false based on whether it was successful

--- src/view/RPGView.java
+++ src/view/RPGView.java
@@ -3,69 +3,116 @@ package view;
 import java.util.Observable;
 import java.util.Observer;
 
+import controller.RPGController;
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import model.Model;
+import model.Tile;
 
 @SuppressWarnings("deprecation")
 public class RPGView extends Application implements Observer {
 	private int mouseX = -1, mouseY = -1;
-	Rectangle[][] backgroundRectangles;
-	GridPane gridPane;
+	private boolean turnOption;
+	private RPGController controller;
+	private Rectangle[][] backgroundRectangles;
+	private GridPane gridPane;
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		// Create the controller this view is based on
+		this.controller =  new RPGController(this);
+		
+		
+		// Create the borderpane and a set of grids inside it
+		// We don't care what color each rectangle is, yet
+		// That will be updated by the model soon
 		BorderPane root = new BorderPane();
 		root.setStyle("-fx-background-color: black;");
-		backgroundRectangles = new Rectangle[16][12];
+		backgroundRectangles = new Rectangle[Model.MAP_SIZE][Model.MAP_SIZE];
 
 		gridPane = new GridPane();
-		for (int x = 0; x < 10; x++) {
-			for (int y = 0; y < 10; y++) {
+		for (int x = 0; x < Model.MAP_SIZE; x++) {
+			for (int y = 0; y < Model.MAP_SIZE; y++) {
 				StackPane stackPane = new StackPane();
-				Rectangle rect = new Rectangle(64, 64);
+				//Rectangle rect = new Rectangle(64, 64);
 				Rectangle background = new Rectangle(40, 40);
 				background.setFill(Color.BLACK);
-				switch ((int) (Math.random() * 5)) {
-				case 0:
-				case 1:
-					rect.setFill(new Color(0.25, 0.33, 0.75, 1));
-					break;
-				case 2:
-					rect.setFill(new Color(0.75, 0.75, 0.25, 1));
-					break;
-				case 3:
-					rect.setFill(new Color(0.33, 0.75, 0.33, 1));
-					break;
-				case 4:
-					rect.setFill(new Color(0.15, 0.5, 0.15, 1));
-					break;
-				}
 				stackPane.getChildren().add(background);
-				stackPane.getChildren().add(rect);
+				//stackPane.getChildren().add(rect);
 				backgroundRectangles[x][y] = background;
 				gridPane.add(stackPane, x, y);
 			}
 		}
 		gridPane.setOnMouseClicked(event -> gridClicked(event));
 		root.setCenter(gridPane);
+	
 
+		createOptionsBar(root);
+
+		
 		// Finalizing
 		stage.setTitle("RPG");
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
-		stage.show();
+		
+		// The model in the controller knows what the map looks like
+		// We can tell the controller to tell the map to 
+		// update the view
+		this.controller.updateView();
+		
+		stage.show(); // Show the stage
+	}
+	
+	private void createOptionsBar(BorderPane root) {
+		GridPane optionsBar = new GridPane();
+		root.setBottom(optionsBar);
+		
+		// Two options
+		// Either build or to move pieces
+		for (int i = 0; i < 1; i++) {
+			ColumnConstraints colConstraint = new ColumnConstraints();
+			colConstraint.setPercentWidth(50);
+			colConstraint.setHalignment(HPos.CENTER);
+			optionsBar.getColumnConstraints().add(colConstraint);
+		}
+		
+		Button buildOption = new Button();
+		buildOption.setText("Build");
+		// When this button is clicked, turnOption is set to true
+		// If turnOption is set to true, it means the player
+		// is trying to build at whatever tile they click
+		buildOption.setOnAction((event) -> {this.turnOption = true;});
+		
+		Button moveUnitOption = new Button();
+		moveUnitOption.setText("Move Unit");
+		// When this button is clicked, turnOption is set to false
+		// So now it means the player wants to move a unit
+		// If later on we see turnOption is set to false, we know
+		// we should let the player select two tiles in succession
+		moveUnitOption.setOnAction((event) -> {this.turnOption = false;});
+			
+		optionsBar.add(buildOption, 0, 0);
+		optionsBar.add(moveUnitOption, 1, 0);
+		
 	}
 
 	private void gridClicked(MouseEvent event) {
 		int mouseX = (int) ((event.getSceneX()) / 42.0);
 		int mouseY = (int) ((event.getSceneY()) / 42.0);
+		
+		Tile tile = this.controller.getTile(mouseX, mouseY);
+		
+		
 		if (this.mouseX != -1 && this.mouseY != -1) {
 			backgroundRectangles[this.mouseX][this.mouseY].setFill(Color.BLACK);
 		}
@@ -88,7 +135,17 @@ public class RPGView extends Application implements Observer {
 	 */
 	@Override
 	public void update(Observable o, Object arg) {
-
+		Tile[][] map = (Tile[][]) arg;
+		for(int i=0; i< Model.MAP_SIZE; i++) {
+			for(int j=0; j< Model.MAP_SIZE; j++) {
+				Tile current = map[i][j];			
+				if(current.getLandType().equals(Tile.DRY_LAND)) {
+					this.backgroundRectangles[i][j].setFill(Color.GREEN);
+				}else if(current.getLandType().equals(Tile.WATER)) {
+					this.backgroundRectangles[i][j].setFill(Color.BLUE);
+				}
+			}
+		}
 	}
 
 }
