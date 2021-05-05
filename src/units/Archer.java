398f87c205a50b3cc651cd3f93ceedc563350e9c
Adi-UA <abanerjee@email.arizona.edu>
2021-05-05 01:42:10 -0700

    Added Javadoc comments for everything

--- src/units/Archer.java
+++ src/units/Archer.java
@@ -3,8 +3,20 @@ package units;
 import model.Nation;
 import view.RPGView;
 
+/**
+ * This class represents an archer unit.
+ * 
+ * @author Adi, Joesph, Shane, Dillion
+ *
+ */
 public class Archer extends Unit {
 
+	/**
+	 * Create an archer
+	 * @param x The column index
+	 * @param y The row index
+	 * @param nation The nation this unit is commanded by
+	 */
 	public Archer(int x, int y, Nation nation) {
 		super(x, y, nation, 20.0, 0.75, 4, 3, RPGView.ARCHER);
 		this.buildCost = ARCHER_BUILD_COST;
--- src/units/Settler.java
+++ src/units/Settler.java
@@ -3,8 +3,20 @@ package units;
 import model.Nation;
 import view.RPGView;
 
+/**
+ * This class represents a settler unit.
+ * 
+ * @author Adi, Joesph, Shane, Dillion
+ *
+ */
 public class Settler extends Unit {
 	
+	/**
+	 * Create a settler
+	 * @param x The column index
+	 * @param y The row index
+	 * @param nation The nation this unit is commanded by
+	 */
 	public Settler(int x, int y, Nation nation) {
 		super(x, y, nation, 0.0, 0.5, 2, 0, RPGView.SETTLER);
 		this.buildCost = SETTLER_BUILD_COST;
